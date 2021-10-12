5ee039c103a3e08f5fe4de1d1d152a0b1e56dd88
Pascal Barlier <contact@codeinter.net>
2021-10-13 00:04:48 +0200

    Add files via upload
    Commande des aiguillages

--- V2/arduino/at-pcc/at-pcc.ino
+++ V2/arduino/at-pcc/at-pcc.ino
@@ -9,6 +9,8 @@
 #include <FlexiTimer2.h>
 #include <EEPROM.h>
 
+// --- Définition des entrées-sorties ---
+
 // MX0 - MX3 : sorties multiplexées
 // -> JLX1 + JLX2 = Colonnes LED + Clavier
 // -> JKX2 + JKX2 = Colonnes clavier
@@ -55,16 +57,94 @@
 #define JLY3PORT PORTL
 #define JLY3PIN PINL
 
-// Tableaux contenant l'état des LED et du clavier
+// --- Etats --
+
+#define DIR 0
+#define DEV 1
+#define ON 1
+#define OFF 0
+
+// ---  Tableaux contenant l'état des périphériques ---
+
+#define MAX_AIG 32 // Nombre de groupes de 8 aiguillages à gérer
+#define NUM_AIG 3 // Nombre de circuits de gestion des aiguillages
+
 byte led[3][4]; // 3 groupes de lignes x 4 colonnes x 8 bits par ligne
 byte key[2][4]; // 2 groupes de lignes x 4 colonnes x 8 bits par ligne
+byte aig[MAX_AIG]; // 32 x 8 = 256 aiguillages
 
-// Macros pour un accès simplifié aux LED et au clavier
+struct _aig_list_
+{
+  byte addr; // Adresse I2C de l'interface
+  byte offset; // Début du stockage dans le tableau aig
+  byte size; // Nombre d'octets utilisés dans le tableau aig
+};
+
+// La valeur de size dépend du type de circuit de gestion des aiguillages :
+// AT-AIG500 : 32 aiguillages -> 4 octets maximum
+// AT-AIG5000 : 60 aiguillages -> 8 octets maximum
+// AT-NANO+PCA9685 : 256 aiguillages -> 32 octets maximum
+// La valeur de offset correspond à la somme des 'size' des circuits précédents
+// -> voir l'exemple ci-dessous
+
+struct _aig_list_ aigList[3]
+{
+  {0x51,0,4}, // circuit AT-AIG500 en adresse 0x51, 4 octets réservés
+  {0x52,4,8}, // circuit AT-AIG5000 en 0x52, l'offset 4 permet de commencer après le circuit précédent
+  {0x53,12,3} // circuit AT-NANO+PCA9685 en 0x53, l'offset vaut 4+8 pour tenir compte des autres circuits
+};
+
+#define aigListSize (sizeof(aigList)/sizeof(*aigList))
+
+// --- Macros pour un accès simplifié aux valeurs stockées ---
+
+#define setLed(n,s) led[0][(n)>>3]=s?(led[0][(n)>>3]|(1<<((n)&7))):(led[0][(n)>>3]&~(1<<((n)&7)))
+// Change l'état d'une LED
 // n = numéro de LED (0-383) GGCCCCBBB (groupe/colonne/bit)
 // s = état de la LED 0=éteinte / 1=allumée 
-#define LED(n,s) led[0][(n)>>3]=s?(led[0][(n)>>3]|(1<<((n)&7))):(led[0][(n)>>3]&~(1<<((n)&7)))
+
+#define getLed(n) ((led[0][(n)>>3]>>((n)&7))&1)
+// Lit l'état d'une LED
 // n = numéro du bouton
-#define KEY(n) ((key[0][(n)>>3]>>((n)&7))&1)
+
+// TODO : LED RGB
+
+#define getKey(n) ((key[0][(n)>>3]>>((n)&7))&1)
+// Lit un bouton
+// n = numéro du bouton
+
+#define getAig(n) (aig[(n)>>3]>>((n)&7))&1)
+// Lit la position d'un aiguillage
+// n = numéro de l'aiguillage
+
+// --- Fonctions d'accès aux circuits périphériques ---
+
+void setAig(word n,byte s)
+// Change la position d'un aiguillage
+// n = numéro de l'aiguillage
+// s = position de l'aiguillage
+{
+  word aigIdx=n>>3;
+  byte aigBit=n&7;
+  byte index;
+  for(index=0; index<aigListSize ; index++)
+    if(aigList[index].offset<=aigIdx) break;
+  if(index<aigListSize) return; // numéro invalide
+  if(aigIdx>aigList[index].offset+aigList[index].size) return; // numéro invalide
+
+  Wire.beginTransmission(aigList[index].addr);
+  Wire.write(s?0x31:0x30); // Commande moveDir ou moveDev
+  Wire.write(n-(aigList[index].offset<<3)); // Numéro de l'aiguillage à commander
+  Wire.endTransmission();
+
+  // Stockage de la position dans le tableau
+  aig[aigIdx]=s?(aig[aigIdx]|(1<<aigBit)):(aig[(n)>>3]&~(1<<aigBit));
+}
+
+// TODO : lecture des détecteurs
+// TODO : accès aux régulateurs
+
+// --- Interruption ---
 
 void interrupt(void)
 // Balayage des matrices clavier et LED
@@ -94,6 +174,8 @@ void interrupt(void)
 
 void setup()
 {
+  Wire.begin();
+
   pinMode(MX0,OUTPUT);
   pinMode(MX1,OUTPUT);
   pinMode(MX2,OUTPUT);
@@ -113,13 +195,26 @@ void setup()
 
   FlexiTimer2::set(1, 0.001, interrupt);
   FlexiTimer2::start();
-
 }
 
 void loop()
 {
-  delay(100);
-  // exemple d'utilisation : le bouton 1 commande la LED 1
-  byte a=KEY(1);
-  LED(1,a);
+  // exemple d'utilisation
+  // le bouton 1 commande la LED 5 et l'aiguillage 7 en voie directe
+  // le bouton 2 commande la LED 6 et l'aiguillage 7 en voie déviée
+  if(getKey(1))
+  {
+    setLed(5,ON);
+    setLed(6,OFF);
+    setAig(7,DIR);
+  }
+  if(getKey(2))
+  {
+    setLed(6,ON);
+    setLed(5,OFF);
+    setAig(7,DEV);
+
+  }
+  delay(10);
 }
+
